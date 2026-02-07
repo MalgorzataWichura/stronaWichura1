2752e747347a96bb7ebeb2e9063fc2c0371ad4d9
GosiaW <mwichura@st.swps.edu.pl>
2026-02-07 15:23:21 +0100

    Dodanie podstrony koszyk z możliwością edycji(dodanie, usunięcie) produktów w nim,  dodanie klawisza "koszyk" na stronach

--- strona/sklep/urls.py
+++ strona/sklep/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('kategoria/miesa/<int:id>', views.szczegolymiesa, name='szczegolymiesa'),
     path('kategoria/pieczywo/<int:id>', views.szczegolypieczywa, name='szczegolypieczywa'),
     path('kategoria/napoje/<int:id>', views.szczegolynapoje, name='szczegolynapoje'),
+    path('kategoria/owoce/<int:id>', views.szczegolyowoce, name='szczegolyowoce'),
+    path('kategoria/warzywa/<int:id>', views.szczegolywarzywa, name='szczegolywarzywa'),
+    path('koszyk/', views.pokaz_koszyk, name='pokaz_koszyk'),
+    path('koszyk/dodaj/<str:kategoria>/<int:id>/', views.dodaj_do_koszyka, name='dodaj_do_koszyka'),
+    path('koszyk/usun/<str:klucz>/', views.usun_z_koszyka, name='usun_z_koszyka'),
 ]
--- strona/sklep/views.py
+++ strona/sklep/views.py
@@ -1,35 +1,74 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Warzywa, Miesa, Napoje, Pieczywo, Owoce
-from django.template import loader
-from django.http import HttpResponse
-def sklep(request): 
-    template = loader.get_template('stronastartowa.html')
-    return HttpResponse(template.render({},request))
-def listaproduktowzkategorii(request, kategoria):
-    tabele = {"warzywa": Warzywa, "miesa": Miesa, "napoje": Napoje, "owoce": Owoce, "pieczywo": Pieczywo}
-    produkty = tabele.get(kategoria)
-    listaproduktowzkategorii = produkty.objects.all()
-    template = loader.get_template('listaproduktowzkategorii.html')
-    context = {"listaproduktowzkategorii":listaproduktowzkategorii, "kategoria":kategoria}
-    return HttpResponse(template.render(context,request))
-def szczegolymiesa(request, id):
-    produkt = get_object_or_404(Miesa, id = id)
-    context = {"produkt":produkt}
-    template = loader.get_template('szczegolymiesa.html')
-    return HttpResponse(template.render(context,request))
-    
-def szczegolypieczywa(request, id):
-    produkt = get_object_or_404(Pieczywo, id = id)
-    context = {"produkt":produkt}
-    template = loader.get_template('szczegolypieczywa.html')
-    return render(request, 'szczegolypieczywa.html', {'produkt': produkt})
+
+# --- WIDOKI STRON PRODUKTÓW ---
 
 def szczegolynapoje(request, id):
     produkt = get_object_or_404(Napoje, id=id)
-    context =  {"produkt":produkt}
-    template = loader.get_template('szczegolynapoje.html')
-    return render(request, 'szczegolynapoje.html', {'produkt': produkt})
+    # Dodajemy 'kategoria': 'napoje', żeby przycisk wiedział gdzie wysłać dane
+    return render(request, 'szczegolynapoje.html', {'produkt': produkt, 'kategoria': 'napoje'})
 
+def szczegolyowoce(request, id):
+    produkt = get_object_or_404(Owoce, id=id)
+    return render(request, 'szczegolyowoce.html', {'produkt': produkt, 'kategoria': 'owoce'})
 
+def szczegolywarzywa(request, id):
+    produkt = get_object_or_404(Warzywa, id=id)
+    return render(request, 'szczegolywarzywa.html', {'produkt': produkt, 'kategoria': 'warzywa'})
 
+def szczegolymiesa(request, id):
+    produkt = get_object_or_404(Miesa, id=id)
+    return render(request, 'szczegolymiesa.html', {'produkt': produkt, 'kategoria': 'miesa'})
+
+def szczegolypieczywa(request, id):
+    produkt = get_object_or_404(Pieczywo, id=id)
+    return render(request, 'szczegolypieczywa.html', {'produkt': produkt, 'kategoria': 'pieczywo'})
+
+# --- WIDOKI KOSZYKA ---
+
+def pokaz_koszyk(request):
+    koszyk = request.session.get('koszyk', {})
+    suma = sum(item['cena'] * item['ilosc'] for item in koszyk.values())
+    return render(request, 'koszyk.html', {'koszyk': koszyk, 'suma': suma})
+
+def dodaj_do_koszyka(request, kategoria, id):
+    tabele = {"warzywa": Warzywa, "miesa": Miesa, "napoje": Napoje, "owoce": Owoce, "pieczywo": Pieczywo}
+    model = tabele.get(kategoria.lower())
+    produkt = get_object_or_404(model, id=id)
+
+    koszyk = request.session.get('koszyk', {})
+    klucz = f"{kategoria}_{id}" 
+
+    if klucz in koszyk:
+        koszyk[klucz]['ilosc'] += 1
+    else:
+        koszyk[klucz] = {
+            'nazwa': produkt.nazwa,
+            'cena': produkt.cena,
+            'ilosc': 1,
+            'kategoria': kategoria,
+            'id': id
+        }
+    
+    request.session['koszyk'] = koszyk
+    request.session.modified = True
+    return redirect('pokaz_koszyk')
+
+def usun_z_koszyka(request, klucz):
+    koszyk = request.session.get('koszyk', {})
+    if klucz in koszyk:
+        del koszyk[klucz]
+        request.session['koszyk'] = koszyk
+        request.session.modified = True
+    return redirect('pokaz_koszyk')
+
+# --- STRONY LISTY ---
+
+def sklep(request): 
+    return render(request, 'stronastartowa.html')
+
+def listaproduktowzkategorii(request, kategoria):
+    tabele = {"warzywa": Warzywa, "miesa": Miesa, "napoje": Napoje, "owoce": Owoce, "pieczywo": Pieczywo}
+    model = tabele.get(kategoria.lower())
+    produkty = model.objects.all() if model else []
+    return render(request, 'listaproduktowzkategorii.html', {"listaproduktowzkategorii": produkty, "kategoria": kategoria})
